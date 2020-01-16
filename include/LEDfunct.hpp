96542029b634e768b99617c1ed32f99f2c4f6567
Christopher Trimble <christopher.trimble@ditchwitch.com>
2020-01-16 13:48:59 -0600

    Added license to all files.

--- include/LEDfunct.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef LEDFUNCT_H
-#define LEDFUNCT_H
-
-void GreenLight(Adafruit_NeoPixel pixel);
-void YellowLight(Adafruit_NeoPixel pixel);
-void RedLight(Adafruit_NeoPixel pixel);
-void LEDoff(Adafruit_NeoPixel pixel);
-
-#endif  //LEDFUNCT_H
