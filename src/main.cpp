650fb65a2c655976d8a54709c40aeaa288ceeae9
Christopher Trimble <christopher.trimble@ditchwitch.com>
2020-02-13 07:50:08 -0600

    Added drawGraph to configServer. Confirmed webserver works.

--- src/main.cpp
+++ src/main.cpp
@@ -195,6 +195,7 @@ void setup() {
   os_timer_arm(&SysTick, 100, true);
 
     configServer.on("/", handleRoot);
+    configServer.on("/test.svg", drawGraph);
     configServer.onNotFound(handleNotFound);
     configServer.begin();
 }
@@ -322,6 +323,25 @@ void handleRoot()
     );
 
     configServer.send(200, "text/html", temp);
+}
+
+void drawGraph() {
+  String out;
+  out.reserve(2600);
+  char temp[70];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x += 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
+
+  configServer.send(200, "image/svg+xml", out);
 }
 
 void handleNotFound()
