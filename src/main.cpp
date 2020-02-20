b00abe20d1e526071c5fdbb5ecdff1708ce18740
Christopher Trimble <christopher.trimble@ditchwitch.com>
2020-02-20 15:59:53 -0600

    Added input test form and handler

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ void setup_wifi();
 void reconnect();
 
 void handleRoot();
-void drawGraph();
+void handlePostTest();
 void handleNotFound();
 
 //===================================
@@ -196,7 +196,7 @@ void setup() {
   os_timer_arm(&SysTick, 100, true);
 
     configServer.on("/", handleRoot);
-    configServer.on("/test.svg", drawGraph);
+    configServer.on("/postTest", handlePostTest);
     configServer.onNotFound(handleNotFound);
     configServer.begin();
 }
@@ -297,17 +297,16 @@ void reconnect()
   client.publish(config.getLWTTopic().c_str(), config.getLWTConnected().c_str());
 }
 
+/*
+  Resource for adding submit forms: https://www.w3schools.com/tags/att_form_method.asp
+*/
 void handleRoot()
 {
     char temp[400];
-    int sec = millis() / 1000;
-    int min = sec / 60;
-    int hr = min / 60;
 
     snprintf(temp, 400,
                "<html>\
   <head>\
-    <meta http-equiv='refresh' content='5'/>\
     <title>ESP8266 Demo</title>\
     <style>\
       body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
@@ -315,34 +314,21 @@ void handleRoot()
   </head>\
   <body>\
     <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
+    <form action=\"/postTest\" method=\"post\">\
+      <label for=\"test\">Test input:</label>\
+      <input type=\"text\" id=\"test\" name=\"test\"><br><br>\
+      <input type=\"submit\" value=\"Submit\">\
   </body>\
-</html>",
-
-           hr, min % 60, sec % 60
+</html>"
     );
 
     configServer.send(200, "text/html", temp);
 }
 
-void drawGraph() {
-  String out;
-  out.reserve(2600);
-  char temp[70];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-
-  configServer.send(200, "image/svg+xml", out);
+void handlePostTest()
+{
+  String textString = configServer.arg("test");
+  Serial.printf("Text box input: %s\n", textString.c_str());
 }
 
 void handleNotFound()
